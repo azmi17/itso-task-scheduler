bc2fccefa3647ea06110c917f6bf1317013c32e2
azmi17 <azmifarhan80@gmail.com>
2022-12-12 14:05:14 +0700

    Revision: reposting saldo apex time config

--- delivery/handler/reposting_lkm_account_apex_by_scheduler.go
+++ delivery/handler/reposting_lkm_account_apex_by_scheduler.go
@@ -20,11 +20,13 @@ func RepostingSaldoApexByScheduler() {
 		schedulerTime := os.Getenv("app.balance_reposting_time")
 
 		task := gocron.NewScheduler(localTime)
-		_, er := task.Every(schedulerTime + "m").Do(usecase.RepostingSaldoApexByScheduler)
+		// _, er := task.Every(schedulerTime + "m").Do(usecase.RepostingSaldoApexByScheduler) // => debug mode
+		_, er := task.Every(1).Day().At(schedulerTime).Do(usecase.RepostingSaldoApexByScheduler)
 		if er != nil {
 			_ = glg.Log(errors.New(er.Error()))
 		}
-		_ = glg.Log("Scheduler INFO: Reposting saldo apex scheduler running at: every", schedulerTime, "minutes")
+		// _ = glg.Log("Scheduler INFO: Reposting saldo apex scheduler running at: every", schedulerTime, "minutes")
+		_ = glg.Log("Scheduler INFO: Reposting saldo apex scheduler running at:", schedulerTime[0:5], "&", schedulerTime[6:11])
 
 		// go RepostingSchedulerRepoObserver()
 
--- helper/app_versioning.go
+++ helper/app_versioning.go
@@ -3,7 +3,7 @@ package helper
 var (
 	AppName        = "ITSO Task Scheduler"
 	AppDescription = "Handle any task with scheduler"
-	AppVersion     = "1.2"
+	AppVersion     = "1.3"
 	AppAuthor      = "Azmi Farhan"
-	LastBuild      = "28/11/2022 10:21"
+	LastBuild      = "12/12/2022 13:26"
 )
